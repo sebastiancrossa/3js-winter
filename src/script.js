1c7db0a2c727e8516e4091eef5e82f68a722f031
Sebastian Crossa <sebastiancrossa@hotmail.com>
2023-01-09 16:07:52 -0700

    feat: add .gitignore file + render three.js

--- src/script.js
+++ src/script.js
@@ -4,26 +4,45 @@ import * as THREE from "three";
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -1.5;
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 1.5;
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
 // Sizes
 const sizes = {
   width: 800,
   height: 600,
 };
 
-// Scene
-const scene = new THREE.Scene();
-
-// Object
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-scene.add(mesh);
-
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 2;
-camera.lookAt(mesh.position);
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.PerspectiveCamera(75, aspectRatio);
+camera.position.z = 5;
+
 scene.add(camera);
 
 // Renderer
@@ -31,3 +50,4 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
